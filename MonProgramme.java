570a6d1c16cedb5fcbc64e9bb5272ea1d5b5d188
scotty <scottyndanga02400@gmail.com>
2026-01-28 13:51:48 +0100

    exercie 1 finit

---
+++ MonProgramme.java
@@ -0,0 +1,10 @@
+
+public class MonProgramme {
+    public static void main(String[] args) {
+        String nom = "Scotty";
+        int age = 25;
+        String ville = "Paris";
+
+        System.out.println("Bonjour, je m'appelle " + nom + ", j'ai " + age + " ans et j'habite à " + ville + "."); 
+    }
+}
