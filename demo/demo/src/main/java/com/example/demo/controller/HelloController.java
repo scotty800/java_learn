d278c29bfe3c68705acfd7f70dce0e9390c9813e
scotty <scottyndanga02400@gmail.com>
2026-02-04 22:13:25 +0100

    execercie

--- demo/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,24 +1,28 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot!";
-    }
-
-    @GetMapping("/")
-    public String root() {
-        return "Welcome!";
-    }
-
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public Map<String, String> ping() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+        return response;
     }
 
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id) {
+        return new User("User" + id, 20 + id);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return user;
+    }
 }
